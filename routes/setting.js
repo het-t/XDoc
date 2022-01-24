e76b6f974929e3439b47fed3247c8398fdda8f5e
het tarkhala <hettarkhala068@gmail.com>
2022-01-24 16:22:37 +0530

    minor

--- routes/setting.js
+++ routes/setting.js
@@ -25,6 +25,7 @@ router.get("/", (req,res,next)=>{
     //code to render columns name as options of dropdown list and username email
     res.render('setting', user)
 })
+
 router.get('/add',(req,res,next)=>{
     queryObj.tablename = user.username;
     queryObj.fieldName = req.query.fieldName;
@@ -41,10 +42,11 @@ router.get('/add',(req,res,next)=>{
 },(req,res,next)=>{
     res.redirect('/user/setting');
 })
+
 router.get('/remove',(req,res,next)=>{
+    
     // first element is fieldName and second element is fieldType
     var fieldToRm = req.query.fieldToRm.split(",");
-    console.log(fieldToRm)
     var fcnfQueryRm = `CALL formCnf(? , ? ,? ,?)`;
     dbConnection.query(fcnfQueryRm , [user.username , 'r' , fieldToRm[0] , fieldToRm[1]], (err,results,fields)=>{
         if (err) console.log(err)
